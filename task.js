7425cfeeceffd4de96728cef1dfbeb4007a35269
OlehTychenko <tychtrade@gmail.com>
2021-03-19 13:29:12 +0200

    add_task_resolved

---
+++ task.js
@@ -0,0 +1,420 @@
+const mainData = [
+    {
+        'group': [
+            {
+                'brand': [
+                    {
+                        'text': 'Coca-Cola',
+                    },
+                ],
+                'price': [
+                    {
+                        'text': ' price 1.95 euro',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola/boisson-gazeuse/p/067000104831',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/h3f/hc9/9704124809246.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse 12x355 mL',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '1',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '1',
+                    },
+                ],
+            },
+            {
+                'brand': [
+                    {
+                        'text': 'Coca-Cola',
+                    },
+                ],
+                'price': [
+                    {
+                        'text': ' price 5,55 euro',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola/boisson-gazeuse/p/067000109836',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/hd8/hc0/9704066351134.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse 6x222 mL',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '2',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '2',
+                    },
+                ],
+            },
+            {
+                'brand': [
+                    {
+                        'text': 'Coca-Cola',
+                    },
+                ],
+                'price': [],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola/boisson-gazeuse/p/067000004278',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/h38/h3c/9704082702366.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse 2 L',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '3',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '3',
+                    },
+                ],
+            },
+            {
+                'brand': [
+                    {
+                        'text': 'Coca-Cola',
+                    },
+                ],
+                'price': [
+                    {
+                        'text': '',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola/boisson-gazeuse/p/067000004629',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/h4a/h4e/9704077361182.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse 500 mL',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '4',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '4',
+                    },
+                ],
+            }
+        ]
+    },
+    {
+        'group': [
+            {
+                'brand': [
+                    {
+                        'text': 'Coke Diète',
+                    },
+                ],
+                'price': [
+                    {
+                        'text': '5$',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola-diete/boisson-gazeuse-sans-cafeine/p/067000004414',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/h7d/hb2/9704072085534.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse sans caféine',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse sans caféine 2 L',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '25',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '25',
+                    },
+                ],
+            },
+            {
+                'brand': [
+                    {
+                        'text': 'Coke Diète',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola-diete/boisson-gazeuse/p/067000009068',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/hc7/he1/9731744333854.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse 1 L',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '26',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '26',
+                    },
+                ],
+            },
+            {
+                'brand': [
+                    {
+                        'text': 'Coca-Cola Zero',
+                    },
+                ],
+                'category': [
+                    {
+                        'text': 'Boissons',
+                    },
+                ],
+                'productUrl': [
+                    {
+                        'text': '/epicerie-en-ligne/allees/boissons/boissons-gazeuses/cola-diete/boisson-gazeuse-sans-calories/p/067000003776',
+                    },
+                ],
+                'thumbnail': [
+                    {
+                        'text': 'https://product-images.metro.ca/images/h8e/h34/9731746463774.jpg',
+                    },
+                ],
+                'name': [
+                    {
+                        'text': 'Boisson gazeuse sans calories',
+                    },
+                ],
+                'nameExtended': [
+                    {
+                        'text': 'Boisson gazeuse sans calories 1 L',
+                    },
+                ],
+                'rankOrganic': [
+                    {
+                        'text': '27',
+                    },
+                ],
+                'rank': [
+                    {
+                        'text': '27',
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+const URL_DOMAIN = 'https://www.metro.ca'
+
+const isProperty = (prop) => {
+    return !!prop
+        && prop.length !== 0
+        && !!prop[0]
+        && prop[0].text.length !== 0
+}
+
+const prepareId = (item, itemName, checker) => {
+    if (checker(item[itemName])) {
+        const res = item[itemName][0].text.match(/\/\d+/);
+
+        return [{ text: res[0].slice(1) }];
+    } else {
+        return [];
+    }
+};
+
+const preparePrice = (item, itemName, checker) => {
+    if (checker(item[itemName])) {
+        const res = item[itemName][0].text.match(/[0-9.,]+/);
+
+        return [{ text: res[0] }];
+    } else {
+        return [];
+    }
+};
+
+const prepareLink = (item, itemName, checker) => {
+    if (checker(item[itemName])) {
+        return [{ text: URL_DOMAIN.concat(item[itemName][0].text) }];
+    } else {
+        return [];
+    }
+};
+
+const prepareRank = (item, itemName, checker) => {
+    if (checker(item[itemName])) {
+        return [{ text: item[itemName][0].text }];
+    } else {
+        return [];
+    }
+}
+
+
+const transform = (data) => {
+    for (let i = 0, len1 = data.length; i < len1; i++) {
+        const itemsBucket = [];
+        for (let j = 0, len2 = data[i].group.length; j < len2; j++) {
+            itemsBucket.push({
+                id: prepareId(data[i].group[j], 'productUrl', isProperty),
+                price: preparePrice(data[i].group[j], 'price', isProperty),
+                productUrl: prepareLink(data[i].group[j], 'productUrl', isProperty),
+                rank: prepareRank(data[i].group[j], 'rank', isProperty)
+            });
+        };
+
+        data[i].group = itemsBucket;
+    };
+
+    return data;
+};
+
+
+
+function render() {
+    const root = document.getElementById('task');
+
+    const result = transform(mainData);
+
+    for (let i = 0, len1 = result.length; i < len1; i++) {
+        const orderedList = document.createElement('ol');
+
+        orderedList.textContent = `Group №${i + 1}`;
+
+        for (let j = 0, len2 = result[i].group.length; j < len2; j++) {
+            const itemWrapper = document.createElement('li');
+            const item = document.createElement('ul');
+            for (let key in result[i].group[j]) {
+                const itemProp = document.createElement('li');
+                if (isProperty(result[i].group[j][key])){
+                itemProp.textContent = `${key}: ${result[i].group[j][key][0].text}`
+                item.append(itemProp);}
+            }
+            itemWrapper.append(item);
+            orderedList.append(itemWrapper);
+        }
+        root.append(orderedList);
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', render);
